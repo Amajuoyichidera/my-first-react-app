a9f4c26f4228a637139c89ae6e14822e94fff61f
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2022-06-08 15:04:18 -0700

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const App = () => {
+      const [counter, setCounter] = useState(0);
+
+      useEffect(() => {
+        alert("you changed the counter to" +      counter)
+      },[counter]);
+
+      return (
+        <div className="App">
+          <button onClick={() => setCounter((prevCount) => prevCount -1 )  }>-</button>
+          <h1>{counter}</h1>
+          <button onClick={() => setCounter((prevCount) => prevCount +1 )  }>+</button>
+        </div>
+      );
+    }
+
+  /* // const Person = (Probs) => {
+    return (
+      <section>
+      <h1>Name: {Probs.name} </h1>
+      <h2>Last Name: {Probs.lastName}</h2>
+      <h2>Age: {Probs.age}</h2>
+      </section>
+    )
+  }
+
+  const App = () => {
+    return (
+     <div className="App">
+      <Person name='chidera' lastName='davido' age='18' />
+      <Person name='chikaodili' lastName='sophia' age='20' />
+     </div>
   );
-}
+  } */
+
+
 
 export default App;
